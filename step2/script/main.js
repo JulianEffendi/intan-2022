fc9b251510b85cc405d3c8c2a7226e9e9f22fa41
JulianEffendi <misakamikoto116@gmail.com>
2022-04-13 12:48:46 +0700

    Tambahkan Foto Dan Beberapa style
    Signed-off-by: JulianEffendi <misakamikoto116@gmail.com>

--- step2/script/main.js
+++ step2/script/main.js
@@ -117,9 +117,9 @@ const animationTimeline = () => {
       "+=0.7"
     )
     .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=7.0")
     .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=4.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
@@ -127,7 +127,7 @@ const animationTimeline = () => {
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=7.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     
@@ -165,11 +165,11 @@ const animationTimeline = () => {
     .from(".idea-7", 0.7, ideaTextTrans)
     .to(".idea-7", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-8", 0.7, ideaTextTrans)
-    .to(".idea-8", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-8", 0.7, ideaTextTransLeave, "+=4.5")
     .from(".idea-9", 0.7, ideaTextTrans)
-    .to(".idea-9", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-9", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-10", 0.7, ideaTextTrans)
-    .to(".idea-10", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-10", 0.7, ideaTextTransLeave, "+=3.5")
     .from(".idea-11", 0.7, ideaTextTrans)
     .to(".idea-11", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-12", 0.7, ideaTextTrans)
